91d6c48ede1436488259ad9cfdee1e073c6eec70
Rownok <rownok2001@gmail.com>
2022-12-08 16:03:04 +0600

    updated color

--- components/Home/Service.js
+++ components/Home/Service.js
@@ -4,24 +4,24 @@ import {IoMdPricetags} from 'react-icons/io'
 import {RiRefundFill} from 'react-icons/ri'
 const Service = () => {
   return (
-    <div className='bg-gradient-to-t from-red-50 to-cyan-50 py-4 px-2 md:px-4'>
-        <p className='text-xl md:text-3xl font-semibold text-center'>Why Book Flight with SkytoGrip</p>
-        <p className='text-lg md:text-xl my-2 text-center text-gray-500'>Book Flight Tickets Online. Save Time and Money!</p>
+    <div className='bg-gradient-to-r from-blue-500 via-cyan-500 to-blue-500 py-2 px-2 md:px-4'>
+        <p className='text-xl md:text-2xl font-semibold text-center text-white'>Why Book Flight with SkytoGrip</p>
+        <p className='text-lg md:text-xl text-center text-gray-500 text-white'>Book Flight Tickets Online. Save Time and Money!</p>
         <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 md:gap-10 py-2">
-            <div className=" flex flex-col gap-2 justify-center items-center">
-                <MdTimelapse className='text-3xl text-primary'/>
-                <p className='text-lg md:text-xl font-semibold'>Fast Booking</p>
-                <p className='text-gray-600 text-center text-sm md:text-base'>Easy Fast Booking Services</p>
+            <div className=" flex flex-col gap-1 justify-center items-center">
+                <MdTimelapse className='text-3xl text-white'/>
+                <p className='text-lg md:text-xl text-white font-semibold'>Fast Booking</p>
+                <p className='text-gray-100 text-center text-sm md:text-base'>Easy Fast Booking Services</p>
             </div>
-            <div className=" flex flex-col gap-2 justify-center items-center">
-                <IoMdPricetags className='text-3xl text-primary'/>
-                <p className='text-lg md:text-xl font-semibold'>Cheapest Price</p>
-                <p className='text-gray-600 text-center text-sm md:text-base'>Get the lowest Price Instead</p>
+            <div className=" flex flex-col gap-1 justify-center items-center">
+                <IoMdPricetags className='text-3xl text-white'/>
+                <p className='text-lg md:text-xl text-white font-semibold'>Cheapest Price</p>
+                <p className='text-gray-100 text-center text-sm md:text-base'>Get the lowest Price Instead</p>
             </div>
-            <div className=" flex flex-col gap-2 justify-center items-center">
-                <RiRefundFill className='text-3xl text-primary'/>
-                <p className='text-lg md:text-xl font-semibold'>Easy cancelation And Refund</p>
-                <p className='text-gray-600 text-center text-sm md:text-base'>Easy Cancel system and money Back!</p>
+            <div className=" flex flex-col gap-1 justify-center items-center">
+                <RiRefundFill className='text-3xl text-white'/>
+                <p className='text-lg md:text-xl text-white font-semibold'>Easy cancelation And Refund</p>
+                <p className='text-gray-100 text-center text-sm md:text-base'>Easy Cancel system and money Back!</p>
             </div>
         </div>
     </div>
