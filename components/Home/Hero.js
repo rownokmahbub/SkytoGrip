aa5e8f0bcf5ab3740d02f9cbdcd38c80a83d8fbc
Rownok <rownok2001@gmail.com>
2022-12-11 11:13:34 +0600

    update search

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -7,14 +7,41 @@ function classNames(...classes) {
 }
 
 const Hero = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+ 
+  const [from, setFrom] = useState({content:""});
+  const [to, setTo] = useState({content:""});
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFrom((prev) => {
+      return {
+        ...prev,
+        content:value
+      };
+    });
+  };
 
+  const handleToChange = (e) =>{
+    const { value } = e.target;
+    setTo((prev) => {
+      return {
+        ...prev,
+        content:value,
+      };
+    });
+  }
+  const handleClick = () => {
+    setFrom(to);
+    setTo(from)
+    
+  }
   return (
     <div className="">
     <div className="bg-gradient-to-r from-red-400 to-cyan-400 py-10">
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-24">
         <div className="max-w-xl mb-10 md:mx-auto px-2 lg:max-w-7xl md:mb-12 bg-gray-50 rounded-xl drop-shadow-xl">
     
-        <Tab.Group>
+        <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
       <Tab.List className='flex gap-5 w-full max-w-sm px-3 py-4 md:px-10 z-10'>
         <Tab className={({ selected }) =>
                 classNames(
@@ -41,16 +68,19 @@ const Hero = () => {
         <div className="flex flex-col gap-1 bg-white px-4 py-2 rounded-xl border border-gray-300 w-full md:w-44 xl:w-52">
                 <p className='text-lg font-medium'>From</p>
              
-                <input className='bg-white py-2 focus:outline-none' type="text" placeholder='Bengaluru..' />
+                <input className='bg-white py-2 focus:outline-none' name='from' type="text" placeholder='Bengaluru..' onChange={handleChange} value={from.content}/>
                
             </div>
+            <button onClick={handleClick}>
             <RiArrowLeftRightFill className='w-10 hidden lg:block'/>
             <RiArrowUpDownFill className='w-10 block lg:hidden'/>
+            </button>
+           
            
             <div className="flex flex-col gap-1 bg-white px-4 py-2 rounded-xl border border-gray-300 w-full md:w-44 xl:w-52">
                 <p className='text-lg font-medium'>To</p>
              
-                <input className='bg-white py-2 focus:outline-none' type="text" placeholder='Bengaluru..' />
+                <input className='bg-white py-2 focus:outline-none' name="to" type="text" placeholder='Bengaluru..' onChange={handleToChange}value={to.content}/>
                
             </div>
 
@@ -80,7 +110,7 @@ const Hero = () => {
         <div className="flex flex-col gap-1 bg-white px-4 py-2 rounded-xl border border-gray-300 w-full md:w-44 xl:w-52">
                 <p className='text-lg font-medium'>From</p>
              
-                <input className='bg-white py-2 focus:outline-none' type="text" placeholder='Bengaluru..' />
+                <input value={from.content } className='bg-white py-2 focus:outline-none' type="text" placeholder='Bengaluru..' />
                
             </div>
             <RiArrowLeftRightFill className='w-10 hidden lg:block'/>
@@ -89,7 +119,7 @@ const Hero = () => {
             <div className="flex flex-col gap-1 bg-white px-4 py-2 rounded-xl border border-gray-300 w-full md:w-44 xl:w-52">
                 <p className='text-lg font-medium'>To</p>
              
-                <input className='bg-white py-2 focus:outline-none' type="text" placeholder='Bengaluru..' />
+                <input value={to.content} className='bg-white py-2 focus:outline-none' type="text" placeholder='Bengaluru..' />
                
             </div>
 
