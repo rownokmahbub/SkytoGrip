c955c8dc50265c68481e228f6fe415519666c188
Rownok <rownok2001@gmail.com>
2022-12-17 23:42:51 +0600

    update

--- components/Home/Terms.js
+++ components/Home/Terms.js
@@ -1,54 +1,84 @@
 import React from "react";
-
+import {HiArrowSmRight} from 'react-icons/hi'
 const Terms = () => {
   return (
     <div className="container mx-auto py-5">
-      <p className="text-xl my-3 font-semibold ">Flight Booking on SkyGoTrip</p>
-      <p className="text-lg text-black ">
-        HappyEasyGo introduces travelers to the best online flight booking
-        experience there is. Every day, thousands of businesspersons and
-        tourists book domestic and international flight tickets through our
-        various platforms. Unbelievably cheap flights and a seamless air ticket
-        booking process make travels merrier than ever.
-      </p>
-      <p className="text-lg text-black ">
-        With HappyEasyGo, you can book flight tickets with sought-after Indian
-        airlines such as IndiGo, Vistara, Go First, Air India and SpiceJet, as
-        well as renowned international air passenger carriers like Thai Airways,
-        Singapore Airlines and Emirates.
-      </p>
-      <p className="text-lg text-black ">
-        
-        HappyEasyGo strives to make travel dreams come true for everyone. If you
-        have got an upcoming travel plan, access the latest offers from
-        HappyEasyGo and take your travel game up a notch.
-      </p>
-      <p className="text-xl my-3 font-semibold ">How can I avail domestic flight offers on SkyGoTrip?</p>
-      <p className="text-lg text-black ">
-        Availing domestic flight offers on ixigo is a simple process that can be
-        completed in a few steps. First, you need to visit the ixigo website and
-        enter your travel dates and destination. You will then be presented with
-        a list of available flights, along with fare information. To view flight
-        offers, simply click on the offers tab at the top of the page. Here
-        you will find a selection of discounts and promotions on domestic
-        flights.
-      </p>
-      <p className="text-xl my-3 font-semibold ">Why Book SkyGoTrip Flight tickets?</p>
+      <div className="flex items-center gap-2">
+        <HiArrowSmRight className="text-xl"/>
+      <p className="text-xl my-3 font-semibold capitalize">Why Book SkyGoTrip Flight tickets?
+</p>
+      </div>
+     
+   
     
-      <li className="text-lg text-black ">kyGoTrip Provide travelers with the best online flight booking experience.</li>
-      <li className="text-lg text-black ">Every day, thousands of business people and tourists book domestic and international air tickets through our different platforms.</li>
-      <li className="text-lg text-black ">Incredibly cheap flights and a seamless flight booking process make traveling easier than ever.</li>
-      <li className="text-lg text-black ">With SkyGoTrip you can book flights on Best Indian airlines like IndiGo, Vistara, Go First, Air India and SpiceJet etc.</li>
-      <li className="text-lg text-black ">SkyGoTrip is dedicated to making everyones travel dreams come true.</li>
+      <li className="text-lg text-black ">SkyGoTrip Provide travelers with the best online flight booking experience.
+</li>
+      <li className="text-lg text-black ">Every day, thousands of business people and tourists book domestic and international air tickets through our different platforms.
+</li>
+      <li className="text-lg text-black ">Incredibly cheap flights and a seamless flight booking process make traveling easier than ever.
+</li>
+      <li className="text-lg text-black ">With SkyGoTrip you can book flights on Best Indian airlines like IndiGo, Vistara, Go First, Air India and SpiceJet etc.
+</li>
+      <li className="text-lg text-black ">SkyGoTrip is dedicated to making everyone travel dreams come true.
+</li>
       <li className="text-lg text-black ">If you have got an upcoming Trip plan, access the latest offers from SkyGoTrip and take your Trip.</li>
      
-      <p className="text-lg text-black my-3 font-semibold ">How can I cancel flight ticket booking SkyGoTrip?</p>
-      <p className="text-lg text-black ">To cancel your air ticket booking, follow the given steps</p>
-      <li className="text-lg text-black ">Visit SkyGoTrip Homepage or open the SkyGotrip app.</li>
-      <li className="text-lg text-black ">Login using the registered mobile no. or email address.Click on your dashboard on the top right corner of the screen, a drop-down menu will open.</li>
-      <li className="text-lg text-black ">Select Your Trips and your booked ticket history will appear.</li>
-      <li className="text-lg text-black ">Click on the flight ticket you wish to cancel.</li>
-      <li className="text-lg text-black ">Select Proceed to cancellation and cancel your ticket.</li>
+      <div className="flex items-center gap-2">
+        <HiArrowSmRight className="text-xl"/>
+      <p className="text-xl my-3 font-semibold capitalize">How can I get Cheapest Flight Tickets on SkyGoTrip?
+
+</p>
+
+      </div>
+      <p className="text-lg font-medium">Enjoy the Cheapest Flight every time in your Trip. Follow this steps to book Flight tickets on the SkyGoTrip website:
+</p>
+<div className="flex items-center gap-2">
+        <HiArrowSmRight className="text-xl"/>
+      <p className="text-xl my-3 font-semibold capitalize">Visit the SkyGoTrip website.
+
+
+</p>
+
+      </div>
+      <p className="text-lg text-black ">Enter the origin, destination, date of trip  clicking on
+</p>
+      <li className="text-lg text-black ">Select flight and the cheapest fare option.
+</li>
+      <li className="text-lg text-black ">Enter passenger details.
+</li>
+      <li className="text-lg text-black ">Apply the promo code and pay for your booking.
+</li>
+      <li className="text-lg text-black ">You can download Ticket payment successful page.
+</li>
+      <li className="text-lg text-black ">The ticket will be sent bye-mail once the payment is complete.
+</li>
+<div className="flex items-center gap-2">
+        <HiArrowSmRight className="text-xl"/>
+      <p className="text-xl my-3 font-semibold capitalize">How can I cancel flight ticket booking SkyGoTrip?
+
+
+
+</p>
+
+      </div>
+      <li className="text-lg text-black ">To cancel your air ticket booking, follow the given steps;
+
+</li>
+      <li className="text-lg text-black ">Visit SkyGoTrip Homepage or open the SkyGotrip app.
+
+</li>
+      <li className="text-lg text-black ">Login using the registered mobile no. or email address.Click on your dashboard on the top right corner of the screen, a drop-down menu will open.
+
+</li>
+      <li className="text-lg text-black ">Select  Your Booking and your booked ticket history will appear.
+
+</li>
+      <li className="text-lg text-black ">Click on the flight ticket you wish to cancel.
+
+</li>
+<li className="text-lg text-black ">Select “Proceed to cancellation” and cancel your ticket.
+
+</li>
     
     </div>
   );
